8381642be61acde6dd8fac648453a9d5cba14a34
devansh <devansh151005@gmail.com>
2024-02-24 21:29:21 +0530

    yes

--- learn/classes.py
+++ learn/classes.py
@@ -3,7 +3,9 @@ class Employee:
     raise_amount= 1.04
     # this is known as a class variable this variable would be used by all the instances of the class if nothing else is specified by the user/ programmer to access it in function self.var_name would be used as it still can be changed for a specific instance of a class for e.g. emp1 and so on and if the class variable like Employee.raise_amount is changed instead of the instance variable emp1.raise_amount the variable would be changed in the entire class and all instances. While if the change is wrt a specific instance. self.var_name would use the instance variable
 
-    # regular class methods
+
+    # regular class methods automatically take self as the first argument we call it self by convention
+
 
     def __init__(self, fname, lname, pay):
         # here self means while initializing the classes emp1= Employee("Name","Last Name", 50000) self automatically does it
@@ -29,8 +31,20 @@ class Employee:
         # here instead of 1.04 which means a fixed 4% increase we should use another variable which can also be changed later.
         self.pay = int(self.pay * self.raise_amount)
         #using int to make it a whole number instead of the float it would make
+    def print_vals(self):
+        print(self.fullname(), self. email, self.pay, sep="\n")
+    @classmethod
+    #this is a class method which takes the first argument as the class itself, by convention we usually take cls as that argument
+    def set_raise_amt(cls, amount):
+        cls.raise_amount= amount
 
-
+    @classmethod
+    def make_object_from_string(cls, emp_str):
+        first, last, pay = emp_str.split("-")
+        # this is the unpacking of the list in first last and pay
+        # return Employee(first, last, pay)
+        return cls(first, last, pay)
+        # both of them can be used as cls is Employee itself as its a class method.
 
 emp1 = Employee("Bill", "Gates", 100000)
 
@@ -84,3 +98,15 @@ print(Employee.num_of_emp) # incremented 3 times by now
 # there are both class variables and class methods
 # and static methods and static variables
 
+
+Employee.set_raise_amt(1.04)
+
+print(Employee.raise_amount)
+print(emp1.raise_amount)
+print(emp2.raise_amount)
+print("\n")
+
+
+emp = "John-Doe-70000"
+new_from_string = Employee.make_object_from_string(emp)
+Employee.print_vals(new_from_string)
