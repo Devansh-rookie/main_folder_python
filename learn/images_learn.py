dc40f276d3e35608fc7795cf99cfce0038019607
devansh <devansh151005@gmail.com>
2024-02-27 18:05:57 +0530

    img

--- learn/images_learn.py
+++ learn/images_learn.py
@@ -1,7 +1,23 @@
 from PIL import Image
+import os
 
-path = "/Users/devansh/Documents/Screenshots/Screenshot 2023-10-08 at 1.43.32 AM.png"
+# image.save("/Users/devansh/Folder_1/Code/Python/main_folder_python/learn/Emilia_Clarke2.png")
 
-image = Image.open(path)
+# image = Image.open("/Users/devansh/Folder_1/Code/Python/main_folder_python/learn/Emilia_Clarke.jpg")
 
-image.show()
+# image.show()
+if not os.path.exists("./learn/pngs_converted_from_jpg"):
+    os.mkdir("./learn/pngs_converted_from_jpg")
+
+if not os.path.exists("./learn/resize"):
+    os.mkdir("./learn/resize")
+size = (400,400)# has to be a tuple in this
+for f in os.listdir("./learn"):# this . means current directory
+    if f.endswith(".jpg") or f.endswith(".png"):
+        # print(f)
+        i = Image.open("./learn/"+f)
+        file_name, file_ext = f.split(".")
+        # print(file_name)
+        i.thumbnail(size)
+        i.save("./learn/resize/{}_resized.{}".format(file_name,".png"))
+    # print(f)
