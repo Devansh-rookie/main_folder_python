8ffa279f5369587e5a65b43734325cbbc55d36f6
devansh <devansh151005@gmail.com>
2024-03-28 11:40:13 +0530

    Online class dated: 28/3/2024, GUIs and Tkinter

---
+++ dr_niyati_baliyan/GUI_28_3_2024.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+window = Tk() # Tk is a class constructor for the window
+
+window.title("tkinter demo")
+window.geometry("400x400") # for size and Grid is for position
+myLabel = Label(window, text="Name: ")
+# grid size is to be given 
+myLabel.grid(row =0, column=0)# row 1 would be below row 0 and column 1 is on the right of column 0
+
+myEntry = Entry(window)
+myEntry.grid(row=0, column=1) # if we put column=0 here then the entry field would overlap the Label that is the previously put there
+
+def clicked():
+    display = "Hello "+ myEntry.get()
+    myLabel.configure(text = display)
+
+# quit can also be used in command to exit
+button = Button(window, text="Click Me", command=clicked, bg="cyan", fg = "red", width= 10)
+button.grid(row=1, column=1)
+var_1 = IntVar()
+var_2 = IntVar()
+var_3 = IntVar()
+var_4 = IntVar()
+Checkbutton(window, text='Python',variable=var_1).grid(row=2,column=1)
+Checkbutton(window, text='C',variable=var_2).grid(row=3,column=1)
+Checkbutton(window, text='C++',variable=var_3).grid(row=4,column=1)
+Checkbutton(window, text='Rust',variable=var_4).grid(row=5,column=1)
+# Radio Buttons one time only one should be selected, check again
+var_5 = IntVar()
+var_6 = IntVar()
+Radiobutton(window, text="OK", variable=var_5).grid(row=6,column=1)
+Radiobutton(window, text="Cancel", variable=var_6).grid(row=7,column=1)
+window.mainloop()
